f21de8aedb7e51a28cd372af591a1ba63402e5e5
Allajah <cat.chat.katze.gatto.gato@gmail.com>
2015-07-09 16:23:56 +0900

    Initial Commit

---
+++ DrawingImage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DrawingImage
+//
+//  Created by RyomaABE on 2015/07/09.
+//  Copyright (c) 2015年 RyomaABE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
